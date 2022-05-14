61005b9e7043cfe449d42b8fbf60f25b468827c2
Hossein113 <hos_m18@yahoo.com>
2022-05-14 14:16:19 -0700

    add Shoes class

---
+++ src/com/homework/jdbc/example/domain/Shoes.java
@@ -0,0 +1,44 @@
+package com.homework.jdbc.example.domain;
+
+public abstract class Shoes {
+
+    private int id;
+
+    private int size;
+
+    private String material;
+
+    private String color;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
