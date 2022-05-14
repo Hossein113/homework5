33ca9a33624e6dc8a81142ff9e3374fbd638a63b
Hossein113 <hos_m18@yahoo.com>
2022-05-14 14:16:45 -0700

    add SportSohe class

---
+++ src/com/homework/jdbc/example/domain/SportSohe.java
@@ -0,0 +1,24 @@
+package com.homework.jdbc.example.domain;
+
+public class SportSohe extends Shoes {
+
+    private String shoeStraType;
+
+    private String sportType;
+
+    public String getShoeStraType() {
+        return shoeStraType;
+    }
+
+    public void setShoeStraType(String shoeStraType) {
+        this.shoeStraType = shoeStraType;
+    }
+
+    public String getSportType() {
+        return sportType;
+    }
+
+    public void setSportType(String sportType) {
+        this.sportType = sportType;
+    }
+}
