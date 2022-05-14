6b9928250524d64e58dd021cff134574de919ef2
Hossein113 <hos_m18@yahoo.com>
2022-05-14 14:17:14 -0700

    add OfficalShoe class

---
+++ src/com/homework/jdbc/example/domain/OfficalShoe.java
@@ -0,0 +1,14 @@
+package com.homework.jdbc.example.domain;
+
+public class OfficalShoe extends Shoes {
+
+    private String followingLayerType;
+
+    public String getFollowingLayerType() {
+        return followingLayerType;
+    }
+
+    public void setFollowingLayerType(String followingLayerType) {
+        this.followingLayerType = followingLayerType;
+    }
+}
