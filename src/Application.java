7cc00c923f790b8d0866a7090fa4c43d2e472748
Hossein113 <hos_m18@yahoo.com>
2022-05-15 12:40:46 -0700

    add Application class

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,15 @@
+import com.homework.jdbc.example.util.ApplicationContext;
+
+import java.sql.SQLException;
+
 public class Application {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws SQLException, ClassNotFoundException {
 
+        ApplicationContext applicationContext = new ApplicationContext();
+
+        applicationContext.getDatabaseCreateTable().init();
+        System.out.println("ok");
 
     }
 }
