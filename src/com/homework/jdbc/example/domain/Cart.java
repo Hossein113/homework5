79092ed4e8c87f831353b39d85c28b62d9734ec7
Hossein113 <hos_m18@yahoo.com>
2022-05-21 12:27:09 -0700

    add Cart class

---
+++ src/com/homework/jdbc/example/domain/Cart.java
@@ -0,0 +1,65 @@
+package com.homework.jdbc.example.domain;
+
+import java.util.List;
+
+public class Cart {
+
+    private int id;
+
+    private User user;
+
+    private int price;
+
+    private Product[] products;
+
+    private List<Product> productList;
+
+    public Cart() {
+    }
+
+    public Cart(int id, User user) {
+        this.id = id;
+        this.user = user;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Product[] getProducts() {
+        return products;
+    }
+
+    public void setProducts(Product[] products) {
+        this.products = products;
+    }
+
+}
