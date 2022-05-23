1dac4995e76c17f689f18574a4a9523bbbb4f398
Hossein113 <hos_m18@yahoo.com>
2022-05-23 15:35:10 -0700

    update Readable Class

--- src/com/homework/jdbc/example/domain/Readable.java
+++ src/com/homework/jdbc/example/domain/Readable.java
@@ -5,25 +5,19 @@ public abstract class Readable extends Product {
 
     private String title;
 
-    public Readable(String name, int price, int number, String title) {
+
+    public Readable(int id, String name, String madeIn, int price, int number, String title) {
+        super(id, name, madeIn, price, number);
         this.title = title;
+    }
+
+    public Readable() {
     }
 
     public Readable(String name, String madeIn, int price, int number, String title) {
         super(name, madeIn, price, number);
         this.title = title;
     }
-
-    public Readable() {
-        this.title = title;
-    }
-
-    public Readable(int id, String name, String madeIn, int price, int number, int id1, String title) {
-        super(id, name, madeIn, price, number);
-
-        this.title = title;
-    }
-
 
     public String getTitle() {
         return title;
@@ -40,4 +34,3 @@ public abstract class Readable extends Product {
                 "} " + super.toString();
     }
 }
-
