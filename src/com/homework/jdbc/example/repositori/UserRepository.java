f2bd598a20b77797299184998008f458f3661155
Hossein113 <hos_m18@yahoo.com>
2022-05-23 15:36:37 -0700

    update UserRepository Class

--- src/com/homework/jdbc/example/repositori/UserRepository.java
+++ src/com/homework/jdbc/example/repositori/UserRepository.java
@@ -49,13 +49,15 @@ public class UserRepository {
         ResultSet resultSet = preparedStatement.executeQuery();
         if (resultSet.next()) {
             return new User(
-                    resultSet.getString(1),
+
+                    resultSet.getInt(1),
+
                     resultSet.getString(2),
                     resultSet.getString(3),
                     resultSet.getString(4),
                     resultSet.getString(5),
-                    resultSet.getString(6)
-
+                    resultSet.getString(6),
+                    resultSet.getString(7)
 
             );
         }
