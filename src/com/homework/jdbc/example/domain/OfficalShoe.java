e9be91402349474d0ec285cc290bb869648c6b3d
Hossein113 <hos_m18@yahoo.com>
2022-05-23 15:34:08 -0700

    update OfficalShoe Class

--- src/com/homework/jdbc/example/domain/OfficalShoe.java
+++ src/com/homework/jdbc/example/domain/OfficalShoe.java
@@ -4,19 +4,13 @@ public class OfficalShoe extends Shoes {
 
     private String followingLayerType;
 
-    public OfficalShoe(String name, String madeIn, int price, int number, int size,
-                       String material, String color, String followingLayerType) {
+    public OfficalShoe(int id, String name, String madeIn, int price, int number, int size, String material, String color, String followingLayerType) {
+        super(id, name, madeIn, price, number, size, material, color);
+        this.followingLayerType = followingLayerType;
+    }
+
+    public OfficalShoe(String name, String madeIn, int price, int number, int size, String material, String color, String followingLayerType) {
         super(name, madeIn, price, number, size, material, color);
-        this.followingLayerType = followingLayerType;
-    }
-
-    public OfficalShoe(String name, int price, int number, int size, String color, String followingLayerType) {
-        super(name, price, number, size, color);
-        this.followingLayerType = followingLayerType;
-    }
-
-    public OfficalShoe(String name, String madeIn, int price, int number, int size, String color, String followingLayerType) {
-        super(name, madeIn, price, number, size, color);
         this.followingLayerType = followingLayerType;
     }
 
